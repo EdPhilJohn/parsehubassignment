2595870945db15b2b8cafa805fd4b1c46038609e
Amal Edward <edphiljohn@gmail.com>
2021-05-05 13:34:54 -0400

    Update DetailsPage.js

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,27 +1,30 @@
 import {Link} from 'react-router-dom'
+import 'bootswatch/dist/superhero/bootstrap.min.css';
 
-const DetailsPage=({value,arrayData,finalArray,path})=>{
+const DetailsPage=({value,arrayData,finalArray,path,index,dt})=>{
+   const send=index;
     const tempArr=[]
     for( let k=0;k<arrayData.length;k++){
       for(let j=0;j<finalArray.length;j++){
       
         if(arrayData[k] === finalArray[j].name){
-          tempArr.push({p:finalArray[j].path,t:finalArray[j].data.type})
+          tempArr.push({p:finalArray[j].path,t:finalArray[j].data.type,j})
 
         }
       }
     }
     return(
       <div>
-  
+       
         <h4>You are at Directory {value} located at {path}</h4>
         <h2>The contents of this directory are</h2>
+       
         
         {
           arrayData.map((value,index)=>{
             
             return(<div key={index}>
-              <Link to={tempArr[index].p}><li style={{listStyle:"none"}}>{!tempArr[index].t?<h3>Directory - {value}</h3>:<h3>FileName - {value}</h3>}</li></Link>
+              <Link onClick={()=>dt.childSend(tempArr[index].j)} to={tempArr[index].p}><li style={{listStyle:"none"}}>{!tempArr[index].t?<h3>Directory - {value}</h3>:<h3>FileName - {value}</h3>}</li></Link>
   
             </div>)
           })
